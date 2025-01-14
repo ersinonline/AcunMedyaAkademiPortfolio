f08a20bb56a251c23dcf3bff27c8f6668cebf825
Ersin Çelik <clk.ersinnn@gmail.com>
2025-01-14 21:40:26 +0300

    Proje dosyası ekle.

---
+++ AcunMedyaAkademiPortfolio/Controllers/StatisticsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AcunMedyaAkademiPortfolio.Models;
+
+namespace AcunMedyaAkademiPortfolio.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        DbPortfolioEntities2 db = new DbPortfolioEntities2();   
+        // GET: Statistics
+        public ActionResult Index()
+        {
+            ViewBag.categoryCount = db.TblCategory.Count();
+            ViewBag.projectCount = db.TblProject.Count();
+            ViewBag.skillCount = db.TblSkill.Count();
+            ViewBag.skillValueAvg = db.TblSkill.Average(x => x.Value);
+            ViewBag.mvcProjectCount = db.TblProject.Where(x => x.ProjectCategory == 1).Count();
+            return View();
+        }
+    }
+}
